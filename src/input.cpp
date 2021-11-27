5d212ccd2f80a88c7e3f40cde619bfb98ab18e64
Robin Jones <robin@networkfusion.co.uk>
2021-11-27 19:35:38 +0000

    Revert input.cpp changes.

--- src/input.cpp
+++ src/input.cpp
@@ -2,15 +2,10 @@
 // SPDX-License-Identifier: MIT
 
 #ifndef LIB_TINY_USB
-//#include <Arduino.h>
-// #include <HID.h>
-// #include <Mouse.h>
-#include <Keyboard.h>
+#include <Arduino.h>
 #include "USBHost_t36.h"
 #else
 // What needs including here for tinyusb?
-// Would also require: https://github.com/cyborg5/TinyUSB_Mouse_and_Keyboard
-#include <TinyUSB_Mouse_and_Keyboard.h> // https://gist.github.com/todbot/13880b3ddfb4f1c857328f1974b12036 may help here!
 #endif
 
 #include "usb64_conf.h"
