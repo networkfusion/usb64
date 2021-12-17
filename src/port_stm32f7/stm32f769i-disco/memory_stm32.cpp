a95d3e17125773661b9a12f46ae186d0f425fe8f
Robin Jones <robin@networkfusion.co.uk>
2021-12-17 15:03:57 +0000

    improve f769i memory

--- src/port_stm32f7/stm32f769i-disco/memory_stm32.cpp
+++ src/port_stm32f7/stm32f769i-disco/memory_stm32.cpp
@@ -6,61 +6,61 @@
 #include "memory.h"
 
 //Pulled in from linker.
-extern uint32_t _extram_start;
-extern uint32_t _extram_end;
+// extern uint32_t _extram_start;
+// extern uint32_t _extram_end;
 
-static uint32_t extram_start = 0;
-static uint32_t extram_end = 0;
+// static uint32_t extram_start = 0;
+// static uint32_t extram_end = 0;
 
 bool memory_dev_init()
 {
-    extram_start = (int)&_extram_start;
-    extram_end = (int)&_extram_end;
+    // extram_start = (int)&_extram_start;
+    // extram_end = (int)&_extram_end;
 
-    //There's 8MB usable SDRAM on the f750-k board located from 0xC0000000
-    //The first 512kBytes are reversed for the LCD. extram_start starts just after this (at 0xC0080000)
-    //I create a new heap at this location for allocating large files
+    // //There's 512Kb usable SDRAM on the f769 board located from 0x20020000
+    // //The first 512kBytes are reversed for the LCD. extram_start starts just after this (at 0xC0080000)
+    // //I create a new heap at this location for allocating large files
     extern SDRAM_HandleTypeDef sdramHandle;
     if (sdramHandle.State != HAL_SDRAM_STATE_READY)
     {
         BSP_SDRAM_Init();
     }
 
-    uint32_t extram_bytes = extram_end - extram_start;
-    ta_init((void *)(extram_start), //Base of heap
-            (void *)(extram_end),   //End of heap
-            extram_bytes / 32768,   //Number of memory chunks (32k/per chunk)
-            16,                     //Smaller chunks than this won't split
-            4);                     //32 word size alignment
+    // uint32_t extram_bytes = extram_end - extram_start;
+    // ta_init((void *)(extram_start), //Base of heap
+    //         (void *)(extram_end),   //End of heap
+    //         extram_bytes / 32768,   //Number of memory chunks (32k/per chunk)
+    //         16,                     //Smaller chunks than this won't split
+    //         4);                     //32 word size alignment
 
-    debug_print_memory("[MEMORY] External memory initialised\n");
-    debug_print_memory("[MEMORY] Detected %ukB\n", extram_bytes / 1024);
-    debug_print_memory("[MEMORY] Heap start: %08x\n", (void *)(extram_start));
-    debug_print_memory("[MEMORY] Heap end: %08x\n", (void *)(extram_end));
-    debug_print_memory("[MEMORY] Number of memory chunks: %u\n", extram_bytes / 32768);
-    return true;
+    // debug_print_memory("[MEMORY] External memory initialised\n");
+    // debug_print_memory("[MEMORY] Detected %ukB\n", extram_bytes / 1024);
+    // debug_print_memory("[MEMORY] Heap start: %08x\n", (void *)(extram_start));
+    // debug_print_memory("[MEMORY] Heap end: %08x\n", (void *)(extram_end));
+    // debug_print_memory("[MEMORY] Number of memory chunks: %u\n", extram_bytes / 32768);
+    // return true;
 }
 
 void *memory_dev_malloc(uint32_t len)
 {
-    void *pt = ta_alloc(len);
-    if (pt == NULL)
-    {
-        debug_print_error("[MEMORY] ERROR, could not allocate memory\n");
-    }
-    return pt;
+    // void *pt = ta_alloc(len);
+    // if (pt == NULL)
+    // {
+    //     debug_print_error("[MEMORY] ERROR, could not allocate memory\n");
+    // }
+    // return pt;
 }
 
 void memory_dev_free(void *add)
 {
-    if ((uint32_t)add >= extram_start)
-    {
-        ta_free(add);
-    }
-    else
-    {
-        free(add);
-    }
+    // if ((uint32_t)add >= extram_start)
+    // {
+    //     ta_free(add);
+    // }
+    // else
+    // {
+         free(add);
+    // }
 }
 
 /*
@@ -71,5 +71,5 @@ void memory_dev_free(void *add)
  */
 uint8_t memory_get_ext_ram_size()
 {
-    return (extram_end - extram_start) / 1024 / 1024;
+    return 0; //(extram_end - extram_start) / 1024 / 1024;
 }
